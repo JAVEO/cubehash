53b2bcf8951e7bd97cd67f2d3b9c0da5880ece8a
Jacek Zadrąg <jacek.zadrag@javeo.eu>
2015-05-05 18:45:47 +0200

    Tests working, passing NIST standard vectors.

--- src/main/scala/eu/javeo/crypto/CubeHash.scala
+++ src/main/scala/eu/javeo/crypto/CubeHash.scala
@@ -1,12 +1,48 @@
 package eu.javeo.crypto
 
-class CubeHash(val bits: Int = 224, val blockLen: Int = 32, val rounds: Int = 16) {
-  private val size  = 32
-  private val halfSize = size / 2
-  private val state = new Array[Int](size)
+/****************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2015 JAVEO Ltd.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*****************************************************************************/
+
+/**
+ * Compute CubeHash digest. This class is not thread safe.
+ *
+ * @param bits Digest bit length
+ * @param blockLen Character block length
+ * @param rounds Number of rounds per block
+ * @param initRounds Number of initial rounds
+ * @param finalRounds Number of final rounds
+ */
+class CubeHash(val bits: Int = 224, val blockLen: Int = 32,
+               val rounds: Int = 16, val initRounds: Int = 160, val finalRounds: Int = 160) {
   assert(bits % 8 == 0, "CubeHash parameter bits should be multiple of 8")
   assert(bits > 0 & bits <= 512, "CubeHash parameter bits should be greater than 0 and less or equal 512")
   assert(blockLen > 0 & blockLen <= 128, "CubeHash parameter blockLen should not exceed 128")
+  private val Size  = 32
+  private val halfSize = Size / 2
+  private val state = new Array[Int](Size)
+  private val padded = new Array[Byte](blockLen)
+  private var paddedLength = 0
   init
 
   private def stateAsBytes: Array[Byte] = {
@@ -20,30 +56,35 @@ class CubeHash(val bits: Int = 224, val blockLen: Int = 32, val rounds: Int = 16
     state(0) = bits / 8
     state(1) = blockLen
     state(2) = rounds
-    for (i <- 3 until size) state(i) = 0
-    round(10)
+    for (i <- 3 until Size) state(i) = 0
+    round(initRounds)
     this
   }
 
-  private def addRotate(r: Int, s: Int) = {
+  private def rotateLeft(value: Int, n: Int): Int = {
+    (value << n) | (value >>> (32-n))
+  }
+
+  private def addRotate(r: Int) = {
     for (i:Int <- 0 until halfSize) {
       state(halfSize + i) += state(i)
-      state(i) = (state(i) << r) ^ (state(i) >>> s)
+      state(i) = rotateLeft(state(i), r)
     }
   }
 
-  private def swapXorSwap(mask1: Int, mask2: Int) = {
+  private def swapXorSwap(mask: Int) = {
+    val mask2 = mask << 2
     for (i:Int <- 0 until halfSize) {
-      if ((i & mask1) != 0) {
-        val j = i ^ mask1
+      if ((i & mask2) != 0) {
+        val j = i ^ mask2
         val tmp = state(i) ^ state(j + halfSize)
         state(i) = state(j) ^ state(i + halfSize)
         state(j) = tmp
       }
     }
-    for (i:Int <- halfSize to size) {
-      if ((i & mask2) != 0) {
-        val j = i ^ mask2
+    for (i:Int <- halfSize until Size) {
+      if ((i & mask) != 0) {
+        val j = i ^ mask
         val tmp = state(i)
         state(i) = state(j)
         state(j) = tmp
@@ -52,47 +93,84 @@ class CubeHash(val bits: Int = 224, val blockLen: Int = 32, val rounds: Int = 16
   }
 
   private def round(n:Int) = {
-    for (i <- 1 to n * rounds) {
-      addRotate(7, 25); swapXorSwap(8, 2)
-      addRotate(11, 21); swapXorSwap(4, 1)
+    for (i <- 1 to n) {
+      addRotate(7); swapXorSwap(2)
+      addRotate(11); swapXorSwap(1)
     }
   }
 
   /**
    * Update state with aligned block of bytes
    */
-  def update(bytes: Array[Byte]) = {
-    assert(bytes.length % blockLen == 0, s"Expected multiples of $blockLen bytes!")
-    val iterator = bytes.sliding(4)
-    while (iterator.hasNext) {
-      for (i <- 0 to blockLen / 4)
-          state(i) ^= CubeHash.bytesToInt(iterator.next())
-      round(1)
+  private def updateBlock(bytes: Array[Byte]) = {
+    assert(bytes.length == blockLen, s"Expected multiple of $blockLen bytes!")
+    val iterator = bytes.sliding(4, 4)
+    for (i <- 0 until blockLen / 4)
+      state(i) ^= CubeHash.bytesToInt(iterator.next())
+    round(rounds)
+  }
+
+  /**
+   * Update state with array of bytes
+   * @param bytes content to update with
+   */
+  def update(bytes: Array[Byte]): Unit = {
+    if (paddedLength > 0) { // take care of block leftovers
+      bytes.copyToArray(padded, paddedLength)
+      if (paddedLength + bytes.length < blockLen)
+        paddedLength += bytes.length
+      else {
+        val bytesCopied = blockLen - paddedLength
+        paddedLength = 0 // block is full, we have to process it
+        updateBlock(padded)
+        if (bytes.length - bytesCopied > 0) update(bytes.slice(bytesCopied, bytes.length))
+      }
+    } else {
+      val iterator = bytes.sliding(blockLen, blockLen)
+      while (iterator.hasNext) {
+        val block = iterator.next()
+        if (block.length == blockLen ) updateBlock(block)
+        else {
+          paddedLength = block.length
+          block.copyToArray(padded)
+        }
+      }
     }
   }
 
   /**
-   * Update state and finalize returning digest value
+   * Update state with a String
+   * @param str content to update with
+   */
+  def update(str: String): Unit = {
+    update(str.getBytes)
+  }
+
+  def update(b: Byte): Unit = {
+    padded(paddedLength) = b
+    paddedLength = (paddedLength + 1) % blockLen
+    if (paddedLength == 0) updateBlock(padded)
+  }
+
+  /**
+   * Get digest value
+   * @param bytes content to digest
    */
   def digest(bytes: Array[Byte]): Array[Byte] = {
-    val padding:Int = bytes.length % blockLen
-    if (padding > 0) {
-      val nopad = bytes.length - padding
-      update(bytes.slice(0, nopad))
-      val padded = new Array[Byte](blockLen)
-      bytes.slice(nopad, bytes.length).copyToArray(padded)
-      padded(padding) = 0x80.toByte
-      update(padded)
-    } else update(bytes)
+    update(bytes)
     digest
   }
 
+  /**
+   * Get digest value
+   * @param str content to digest
+   */
   def digest(str: String): Array[Byte] = {
     digest(str.getBytes)
   }
 
   /**
-   * Take a look at state (for testing)
+   * Take a look at digest state (for testing)
    */
   def peek: Array[Byte] = {
     stateAsBytes.slice(0, bits / 8)
@@ -102,8 +180,12 @@ class CubeHash(val bits: Int = 224, val blockLen: Int = 32, val rounds: Int = 16
    * Finalize returning digest value
    */
   def digest: Array[Byte] = {
-    state(size-1) ^= 1
-    round(10)
+    padded(paddedLength) = 0x80.toByte
+    for (i <- paddedLength + 1 until blockLen) padded(i) = 0
+    paddedLength = 0
+    updateBlock(padded)
+    state(Size-1) ^= 1
+    round(finalRounds)
     peek
   }
 }
@@ -122,5 +204,19 @@ object CubeHash {
       ((bytes(3) & 0xff) << 24)
   }
 
-  def hexValueOf(buf: Array[Byte]): String = buf.map("%02X" format _).mkString
+  def hexValueOf(buf: Array[Byte]): String = buf.map("%02x" format _).mkString
+
+  // Standard parameter values as defined by djb
+  def SH192:CubeHash = new CubeHash(192)
+  def SH224:CubeHash = new CubeHash(224)
+  def SH256:CubeHash = new CubeHash(256)
+  def SH512:CubeHash = new CubeHash(512)
+  def HS128:CubeHash = new CubeHash(128,32,16,16,32)
+  def HS160:CubeHash = new CubeHash(160,32,16,16,32)
+  def HS192:CubeHash = new CubeHash(192,32,16,16,32)
+  def HS224:CubeHash = new CubeHash(224,32,16,16,32)
+  def HS256:CubeHash = new CubeHash(256,32,16,16,32)
+  def HS384:CubeHash = new CubeHash(384,32,16,16,32)
+  def HS512:CubeHash = new CubeHash(512,32,16,16,32)
+  def HS512x:CubeHash = new CubeHash(512,1,16,16,32)
 }
